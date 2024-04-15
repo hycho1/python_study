503af65269aa57a16820ba30afed5c36afb5eaf9
hycho1 <3siptn7526@gmail.com>
2024-04-15 17:25:41 +0900

    commit_list turn

--- python_0415/note1.py
+++ python_0415/note1.py
@@ -7,9 +7,6 @@ print("a + b : ", a + b)
 print("a + b : ", a + b)
 print("a / b : ", a / b)
 print("a // b : ", a // b)
-<<<<<<< Updated upstream
-print("a % b : ", a % b)
-=======
 print("a % b : ", a % b)
 
 c= 2
@@ -54,10 +51,41 @@ f_strr = "{2:(>5}{1:<5}{0:<5} ".format("hi", "yes", "yeet")
 
 print(f_strr)
 '''
-
+'''
 
 
 a = "Life is too short"
 
-print(a.upper().lower().replace(" ",",").split(","))
->>>>>>> Stashed changes
+b = a.upper()
+c = b.lower()
+d = c.replace(" ",",")
+print(a.upper())
+print(b.lower())
+print(c.replace(" ",","))
+print(d.split(","))
+
+'''
+
+#odd = [1, 3, [7, 9], 5]
+#odd.sort()
+#print(odd)
+
+
+str = "0 똘기 떵이 호치 새초미"
+
+list1 = str.split(" ");
+
+cnt = int(list1[0])
+list1.pop(0)
+
+
+if(int(cnt) >= 0):
+    for i in range(cnt):
+        list1.insert(0,list1.pop())
+else:
+    cnt *= -1
+    for i in range(cnt):
+        list1.insert(len(list1)-1 ,list1.pop(0))
+
+print(list1)
+
