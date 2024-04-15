af1d2f0b971ac15d8c63c86df2948633bb4ed8c8
hycho1 <3siptn7526@gmail.com>
2024-04-15 11:35:05 +0900

    commit_3

--- python_0415/note1.py
+++ python_0415/note1.py
@@ -6,3 +6,15 @@ print("a + b : ", a + b)
 print("a / b : ", a / b)
 print("a // b : ", a // b)
 print("a % b : ", a % b)
+
+c= 2
+c **= 3;
+print(c)
+
+mult = '''
+multiline
+test
+string
+'''
+
+print(mult)
