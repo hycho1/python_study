917fb00f4d8df5151e86998f58366fafa1a0b2fd
hycho1 <3siptn7526@gmail.com>
2024-04-15 15:40:17 +0900

    commit_4

--- python_0415/note1.py
+++ python_0415/note1.py
@@ -1,3 +1,5 @@
+
+'''
 a = 10
 b = 2
 
@@ -6,3 +8,52 @@ print("a + b : ", a + b)
 print("a / b : ", a / b)
 print("a // b : ", a // b)
 print("a % b : ", a % b)
+
+c= 2
+c **= 3;
+print(c)
+
+mult = """
+multiline 
+test
+string
+"""
+
+print("===")
+print(mult[0])
+print("===")
+print("\n")
+print("===")
+print()
+print("===")
+
+a = "20230331Rainy"
+year = a[0:4]
+month = a[4:6]
+date = a[6:8]
+
+weather = a[8:]
+
+print(year + month + date + weather)
+
+
+'''
+'''
+num = 99
+width = 10
+f_str = f"{num:>{10}}"
+
+print(f_str)
+
+
+
+f_strr = "{2:(>5}{1:<5}{0:<5} ".format("hi", "yes", "yeet")
+
+print(f_strr)
+'''
+
+
+
+a = "Life is too short"
+
+print(a.upper().lower().replace(" ",",").split(","))
